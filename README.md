57377b0f165b5b2ce2f3e600c133ccf7cdc5c57e
mz13110 <115488309+mz13110@users.noreply.github.com>
2022-10-10 11:10:44 -0700

    import

---
+++ README.md
@@ -0,0 +1,41 @@
+# Metallic
+An amazing new proxy site with lots of customization. Made for TN's proxathon.
+
+## Features
+Simple clean design
+
+React framework
+
+Super simple frontend with omnibox
+
+Settings (icon) with themes
+
+Mobile support
+
+TOMP proxies
+
+About:Blank cloaking with A:BC
+
+PWA app
+
+Advanced customization
+
+Settings:
+
+Search engine
+
+Advanced tab cloaking (preset, url and custom)
+
+Lots of themes
+
+Custom theme colors
+
+Change background particles
+
+Omnibox with suggestions
+
+Lots of games and apps
+
+Obfuscated keywords
+
+Adblocking
---
+++ src/index.mjs
@@ -0,0 +1,68 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import createBareServer from '@tomphttp/bare-server-node';
+import http from 'http';
+import express from 'express';
+
+const app = express();
+const server = http.createServer();
+import path from 'path';
+const __dirname = path.resolve();
+const port = process.env.PORT || 8080;
+
+if (!fs.existsSync("build")) {
+    console.log("Building Metallic")
+    execSync("npm run build")
+	fs.writeFileSync(__dirname + "/build/404.html", fs.readFileSync(__dirname + "/build/index.html"));
+}
+
+app.use(express.static("build"));
+
+const bareServer = createBareServer('/bare/', {
+	logErrors: false,
+	localAddress: undefined,
+	project: {
+		name: 'Metallic',
+		repository: 'https://github.com/Nebelung-dev/Metallic',
+	},
+});
+
+server.on('request', (req, res) => {
+  console.log(req.url)
+  if(req.url === "/") {
+    if(req.headers.cookie && req.headers.cookie.includes("enable=true")) {
+      app(req, res);
+    }
+    else {
+      console.log("failed")
+      /*
+      if(req.headers.dnt === "1") {
+        res.setHeader("Set-Cookie", "enable=true")
+      }*/
+      if(req.url === "/") res.writeHead(200).write(fs.readFileSync("build/mask.html"))
+      else res.writeHead(404);
+      res.end();
+    }
+  }
+  else if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  }
+  else {
+    app(req, res);
+  }
+
+});
+
+server.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+server.on('listening', () => {
+	console.log('Metallic running on port ' + port);
+});
+
+server.listen(port);
